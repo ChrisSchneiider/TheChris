4be66488e15b5f3b476d3706a9a857d827cb5e67
ChrisSchneider <chrys.destiny@live.com>
2021-11-17 04:26:46 -0300

    { build } -remove

--- root/_nuxt/32f9567.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{273:function(t,n,e){"use strict";e.r(n);var l=e(57),component=Object(l.a)({},(function(){var t=this,n=t.$createElement,e=t._self._c||n;return e("div",[e("NuxtLink",{attrs:{to:"/"}},[t._v("voltar")])],1)}),[],!1,null,null,null);n.default=component.exports}}]);
